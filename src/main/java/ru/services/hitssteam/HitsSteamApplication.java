db5e61db39a1e1dde6459c2daad4505e33543615
Several_SIZE <kner647@gmail.com>
2026-06-12 12:08:56 +0700

    Initial commit

---
+++ src/main/java/ru/services/hitssteam/HitsSteamApplication.java
@@ -0,0 +1,13 @@
+package ru.services.hitssteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitsSteamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HitsSteamApplication.class, args);
+    }
+
+}
